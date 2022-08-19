45800860edaed7fbd33f4b14c374c739a6e6280a
rainfall-pankaj <pankaj@rainfall.one>
2022-08-19 14:06:51 +0530

    Added typescript file and Yaml file

---
+++ greetPerson.js
@@ -0,0 +1,10 @@
+function greeter(person) {
+    return "Hello! " + person;
+}
+var user = 'Pankaj Shinde';
+console.log(greeter(user));
+function add(a, b) {
+    return a + b;
+}
+var x = 10, y = 20;
+console.log(add(x, y));
---
+++ greetPerson.ts
@@ -0,0 +1,12 @@
+function greeter(person){
+    return "Hello! "+person;
+}
+let user = 'Pankaj Shinde';
+console.log(greeter(user));
+
+function add(a, b)
+{
+    return a+b;
+}
+let x=10, y=20;
+console.log(add(x,y));
